236ceeb24476196d05dfebc21c63dde76c8c435b
MikeDiam <info@mdiamond.ru>
2023-01-10 21:42:47 +0700

    [deploy fix] fix metadata handler

--- src/entities/metadata.ts
+++ src/entities/metadata.ts
@@ -4,35 +4,37 @@ import { Vault } from '../../generated/schema'
 
 
 export function updateMetadata(metadata: JSONValue, vaultAddress: Value): void {
-  const json = metadata.toObject()
-  const vault = Vault.load(vaultAddress.toString())
+  if (metadata) {
+    const json = metadata.toObject()
+    const vault = Vault.load(vaultAddress.toString())
 
-  const imageUrl = json.get('imageUrl')
-  const displayName = json.get('displayName')
-  const description = json.get('description')
+    const imageUrl = json.get('imageUrl')
+    const displayName = json.get('displayName')
+    const description = json.get('description')
 
-  if (vault) {
-    if (description) {
-      const descriptionString = description.toString()
-      const isDescriptionValid = descriptionString.length <= 1000
+    if (vault) {
+      if (description) {
+        const descriptionString = description.toString()
+        const isDescriptionValid = descriptionString.length <= 1000
 
-      vault.description = isDescriptionValid ? descriptionString : ''
+        vault.description = isDescriptionValid ? descriptionString : ''
+      }
+
+      if (displayName) {
+        const displayNameString = displayName.toString()
+        const isDisplayNameValid = displayNameString.length <= 30
+
+        vault.displayName = isDisplayNameValid ? displayNameString : ''
+      }
+
+      if (imageUrl) {
+        const imageUrlString = imageUrl.toString()
+        const isImageUrlValid = imageUrlString.startsWith('https://static.stakewise.io/')
+
+        vault.imageUrl = isImageUrlValid ? imageUrlString : ''
+      }
+
+      vault.save()
     }
-
-    if (displayName) {
-      const displayNameString = displayName.toString()
-      const isDisplayNameValid = displayNameString.length <= 30
-
-      vault.displayName = isDisplayNameValid ? displayNameString : ''
-    }
-
-    if (imageUrl) {
-      const imageUrlString = imageUrl.toString()
-      const isImageUrlValid = imageUrlString.startsWith('https://static.stakewise.io/')
-
-      vault.imageUrl = isImageUrlValid ? imageUrlString : ''
-    }
-
-    vault.save()
   }
 }
