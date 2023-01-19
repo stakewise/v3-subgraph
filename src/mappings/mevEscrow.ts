c3b09f4d504c0d0fb22552a819868202f5a6103a
MikeDiam <info@mdiamond.ru>
2023-01-19 17:40:01 +0700

    [snapshots] update mev escrow

--- src/mappings/mevEscrow.ts
+++ src/mappings/mevEscrow.ts
@@ -1,22 +1,28 @@
-import { Vault } from '../../generated/schema'
+import { MevEscrow, Vault } from '../../generated/schema'
 import { MevReceived } from '../../generated/templates/MevEscrow/MevEscrow'
 import { createOrLoadDaySnapshot, getRewardPerAsset } from '../entities/daySnapshot'
 
 
 export function handleMevReceived(event: MevReceived): void {
-  const vaultId = event.address.toHex()
-  const vault = Vault.load(vaultId) as Vault
+  const mevEscrowAddress = event.address.toHex()
 
-  const reward = event.params.amount
+  const mevEscrow = MevEscrow.load(mevEscrowAddress)
 
-  vault.executionReward = vault.executionReward.plus(reward)
-  vault.totalAssets = vault.totalAssets.plus(reward)
-  vault.save()
+  if (mevEscrow) {
+    const vaultId = mevEscrow.vault
+    const vault = Vault.load(vaultId) as Vault
 
-  const daySnapshot = createOrLoadDaySnapshot(event.block.timestamp, vaultId)
-  const rewardPerAsset = getRewardPerAsset(reward, vault.feePercent, daySnapshot.principalAssets)
+    const reward = event.params.amount
 
-  daySnapshot.rewardPerAsset = daySnapshot.rewardPerAsset.plus(rewardPerAsset)
-  daySnapshot.totalAssets = daySnapshot.totalAssets.plus(reward)
-  daySnapshot.save()
+    vault.executionReward = vault.executionReward.plus(reward)
+    vault.totalAssets = vault.totalAssets.plus(reward)
+    vault.save()
+
+    const daySnapshot = createOrLoadDaySnapshot(event.block.timestamp, vaultId)
+    const rewardPerAsset = getRewardPerAsset(reward, vault.feePercent, daySnapshot.principalAssets)
+
+    daySnapshot.rewardPerAsset = daySnapshot.rewardPerAsset.plus(rewardPerAsset)
+    daySnapshot.totalAssets = daySnapshot.totalAssets.plus(reward)
+    daySnapshot.save()
+  }
 }
