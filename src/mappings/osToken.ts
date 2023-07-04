fe8c93356a6c21ec96c6c158c710a40b28fc6ff6
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-07-04 20:56:01 +0300

    Fix OsToken snapshot

--- src/mappings/osToken.ts
+++ src/mappings/osToken.ts
@@ -5,10 +5,13 @@ import { OsTokenSnapshot } from '../../generated/schema'
 export function handleAvgRewardPerSecondUpdated(event: AvgRewardPerSecondUpdated): void {
   const newAvgRewardPerSecond = event.params.avgRewardPerSecond
   const txHash = event.transaction.hash.toHex()
-  const allocatorAction = new OsTokenSnapshot(`${txHash}-${event.transactionLogIndex.toString()}`)
-  allocatorAction.avgRewardPerSecond = newAvgRewardPerSecond
-  allocatorAction.createdAt = event.block.timestamp
-  allocatorAction.save()
+  const snapshot = new OsTokenSnapshot(`${txHash}-${event.transactionLogIndex.toString()}`)
+  snapshot.avgRewardPerSecond = newAvgRewardPerSecond
+  snapshot.createdAt = event.block.timestamp
+  snapshot.save()
 
-  log.info('[OsToken] AvgRewardPerSecondUpdated avgRewardPerSecond={}', [newAvgRewardPerSecond.toString()])
+  log.info('[OsToken] AvgRewardPerSecondUpdated avgRewardPerSecond={} createdAt={}', [
+    newAvgRewardPerSecond.toString(),
+    snapshot.createdAt.toString(),
+  ])
 }
