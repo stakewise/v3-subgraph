c562842d695660ef188617d7ea2b48b81bab90c4
Mike Diamond <info@mdiamond.ru>
2024-12-18 23:16:29 +0300

    [fiat rates] add CNY, JPY, KRW and AUD rates (#89)
    * Refactor rewards calculation
    
    * [fiat rates] add CNY, JPY, KRW and AUD rates
    
    ---------
    
    Co-authored-by: Dmitri Tsumak <tsumak.dmitri@gmail.com>

--- src/mappings/exchangeRates.ts
+++ src/mappings/exchangeRates.ts
@@ -4,6 +4,10 @@ import {
   ASSETS_USD_PRICE_FEED,
   EUR_USD_PRICE_FEED,
   GBP_USD_PRICE_FEED,
+  CNY_USD_PRICE_FEED,
+  JPY_USD_PRICE_FEED,
+  KRW_USD_PRICE_FEED,
+  AUD_USD_PRICE_FEED,
   DAI_USD_PRICE_FEED,
   USDC_USD_PRICE_FEED,
   SWISE_ASSET_UNI_POOL,
@@ -20,6 +24,10 @@ export function handleExchangeRates(block: ethereum.Block): void {
   let usdcUsdRate = BigDecimal.zero()
   let eurToUsdRate = BigDecimal.zero()
   let gbpToUsdRate = BigDecimal.zero()
+  let cnyToUsdRate = BigDecimal.zero()
+  let jpyToUsdRate = BigDecimal.zero()
+  let krwToUsdRate = BigDecimal.zero()
+  let audToUsdRate = BigDecimal.zero()
   let response: BigInt
   let priceFeedContract: PriceFeedContract
 
@@ -42,6 +50,34 @@ export function handleExchangeRates(block: ethereum.Block): void {
     priceFeedContract = PriceFeedContract.bind(gbpUsdPriceFeed)
     response = priceFeedContract.latestAnswer()
     gbpToUsdRate = new BigDecimal(response).div(decimals)
+  }
+
+  const cnyUsdPriceFeed = Address.fromString(CNY_USD_PRICE_FEED)
+  if (cnyUsdPriceFeed.notEqual(Address.zero())) {
+    priceFeedContract = PriceFeedContract.bind(cnyUsdPriceFeed)
+    response = priceFeedContract.latestAnswer()
+    cnyToUsdRate = new BigDecimal(response).div(decimals)
+  }
+
+  const jpyUsdPriceFeed = Address.fromString(JPY_USD_PRICE_FEED)
+  if (jpyUsdPriceFeed.notEqual(Address.zero())) {
+    priceFeedContract = PriceFeedContract.bind(jpyUsdPriceFeed)
+    response = priceFeedContract.latestAnswer()
+    jpyToUsdRate = new BigDecimal(response).div(decimals)
+  }
+
+  const krwUsdPriceFeed = Address.fromString(KRW_USD_PRICE_FEED)
+  if (krwUsdPriceFeed.notEqual(Address.zero())) {
+    priceFeedContract = PriceFeedContract.bind(krwUsdPriceFeed)
+    response = priceFeedContract.latestAnswer()
+    krwToUsdRate = new BigDecimal(response).div(decimals)
+  }
+
+  const audUsdPriceFeed = Address.fromString(AUD_USD_PRICE_FEED)
+  if (audUsdPriceFeed.notEqual(Address.zero())) {
+    priceFeedContract = PriceFeedContract.bind(audUsdPriceFeed)
+    response = priceFeedContract.latestAnswer()
+    audToUsdRate = new BigDecimal(response).div(decimals)
   }
 
   const daiUsdPriceFeed = Address.fromString(DAI_USD_PRICE_FEED)
@@ -71,12 +107,20 @@ export function handleExchangeRates(block: ethereum.Block): void {
   const one = BigDecimal.fromString('1')
   const usdToEurRate = eurToUsdRate.gt(zero) ? one.div(eurToUsdRate) : zero
   const usdToGbpRate = gbpToUsdRate.gt(zero) ? one.div(gbpToUsdRate) : zero
+  const usdToCnyRate = cnyToUsdRate.gt(zero) ? one.div(cnyToUsdRate) : zero
+  const usdToJpyRate = jpyToUsdRate.gt(zero) ? one.div(jpyToUsdRate) : zero
+  const usdToKrwRate = krwToUsdRate.gt(zero) ? one.div(krwToUsdRate) : zero
+  const usdToAudRate = audToUsdRate.gt(zero) ? one.div(audToUsdRate) : zero
 
   const network = loadNetwork()!
   network.assetsUsdRate = assetsUsdRate
   network.swiseUsdRate = swiseUsdRate
   network.usdToEurRate = usdToEurRate
   network.usdToGbpRate = usdToGbpRate
+  network.usdToCnyRate = usdToCnyRate
+  network.usdToJpyRate = usdToJpyRate
+  network.usdToKrwRate = usdToKrwRate
+  network.usdToAudRate = usdToAudRate
   network.daiUsdRate = daiUsdRate
   network.usdcUsdRate = usdcUsdRate
   network.save()
@@ -89,14 +133,22 @@ export function handleExchangeRates(block: ethereum.Block): void {
   exchangeRateSnapshot.usdcUsdRate = usdcUsdRate
   exchangeRateSnapshot.usdToEurRate = usdToEurRate
   exchangeRateSnapshot.usdToGbpRate = usdToGbpRate
+  exchangeRateSnapshot.usdToCnyRate = usdToCnyRate
+  exchangeRateSnapshot.usdToJpyRate = usdToJpyRate
+  exchangeRateSnapshot.usdToKrwRate = usdToKrwRate
+  exchangeRateSnapshot.usdToAudRate = usdToAudRate
   exchangeRateSnapshot.save()
 
   log.info(
-    '[ExchangeRates] assetsUsdRate={} usdToEurRate={} usdToGbpRate={} daiUsdRate={} usdcUsdRate={} swiseUsdRate={}',
+    '[ExchangeRates] assetsUsdRate={} usdToEurRate={} usdToGbpRate={} usdToCnyRate={} usdToJpyRate={} usdToKrwRate={} usdToAudRate={} daiUsdRate={} usdcUsdRate={} swiseUsdRate={}',
     [
       assetsUsdRate.toString(),
       usdToEurRate.toString(),
       usdToGbpRate.toString(),
+      usdToCnyRate.toString(),
+      usdToJpyRate.toString(),
+      usdToKrwRate.toString(),
+      usdToAudRate.toString(),
       daiUsdRate.toString(),
       usdcUsdRate.toString(),
       swiseUsdRate.toString(),
