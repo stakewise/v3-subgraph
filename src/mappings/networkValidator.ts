7ab9755e79cae3f25af0adf78b2c1eb3acd518e0
Alexander Sysoev <avsysoev60@gmail.com>
2025-01-29 23:18:46 +0300

    Network validators (#102)
    * Add network validators to schema
    
    * Network validators: Use bytes as id
    
    * Load genesis validators from IPFS
    
    * Update genesis ipfs hashes
    
    * Update src/mappings/networkValidator.ts
    
    Signed-off-by: Dmitri Tsumak <tsumak.dmitri@gmail.com>
    
    * Review log fixes
    
    ---------
    
    Signed-off-by: evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
    Signed-off-by: Dmitri Tsumak <tsumak.dmitri@gmail.com>
    Co-authored-by: evgeny-stakewise <123374581+evgeny-stakewise@users.noreply.github.com>
    Co-authored-by: Dmitri Tsumak <tsumak.dmitri@gmail.com>

---
+++ src/mappings/networkValidator.ts
@@ -0,0 +1,41 @@
+import {Bytes, ethereum, ipfs, json, log} from '@graphprotocol/graph-ts'
+import { NetworkValidator, OsTokenExitRequest } from '../../generated/schema'
+import { DepositEvent } from '../../generated/ValidatorsRegistry/ValidatorsRegistry'
+import { GENESIS_IPFS_HASH } from '../helpers/constants'
+
+export function handleDepositEvent(event: DepositEvent): void {
+  const publicKey = event.params.pubkey
+  let networkValidator = NetworkValidator.load(publicKey)
+  if (networkValidator == null) {
+    networkValidator = new NetworkValidator(publicKey)
+    networkValidator.save()
+  }
+
+  log.info('[NetworkValidator] DepositEvent publicKey={}', [publicKey.toHex()])
+}
+
+export function handleGenesisValidators(block: ethereum.Block): void {
+  log.info('[NetworkValidator] Start genesis validators processing...', [])
+
+  if (!GENESIS_IPFS_HASH){
+    log.info('[NetworkValidator] Empty genesis validators hash', [])
+    return
+  }
+  let data: Bytes | null = ipfs.cat(GENESIS_IPFS_HASH)
+  while (!data) {
+    log.warning('[NetworkValidator] ipfs.cat failed for genesis validators hash={}, retrying', [GENESIS_IPFS_HASH])
+    data = ipfs.cat(GENESIS_IPFS_HASH)
+  }
+
+  for (let i = 0; i < data!.length; i=i+48) {
+    let publicKey =  Bytes.fromUint8Array(data!.slice(i, i + 48))
+    let networkValidator = NetworkValidator.load(publicKey)
+    if (networkValidator == null) {
+      networkValidator = new NetworkValidator(publicKey)
+      networkValidator.save()
+    }
+  }
+  log.info('[NetworkValidator] Successfully processed genesis validators for hash {}', [
+    GENESIS_IPFS_HASH
+  ])
+}
