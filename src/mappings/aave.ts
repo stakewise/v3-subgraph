99106ac8280137c5574e6d64c39f8f885c485224
MikeDiam <info@mdiamond.ru>
2025-04-21 17:06:37 +0300

    [aave data] update aave handlers

--- src/mappings/aave.ts
+++ src/mappings/aave.ts
@@ -1,32 +1,69 @@
-import { Address } from '@graphprotocol/graph-ts'
-import { Transfer } from '../../generated/AaveToken/AaveToken'
+import { Address, BigInt } from '@graphprotocol/graph-ts'
+import { BalanceTransfer, Burn, Mint } from '../../generated/AaveToken/AaveToken'
 import { SupplyCapChanged } from '../../generated/AavePoolConfigurator/AavePoolConfigurator'
-
 import { loadAave } from '../entities/aave'
+import { OS_TOKEN } from '../helpers/constants'
 
 export function handleSupplyCapChanged(event: SupplyCapChanged): void {
+  if (event.params.asset.equals(OS_TOKEN)) {
+    const aave = loadAave()!
+
+    aave.osTokenSupplyCap = event.params.newSupplyCap
+    aave.save()
+  }
+}
+
+function mint(value: BigInt, balanceIncrease: BigInt, onBehalf: Address): void {
+  const onBehalfString = onBehalf.toHexString().toLowerCase()
+
+  const isMintingToTreasury =
+    onBehalfString != '0xb2289e329d2f85f1ed31adbb30ea345278f21bcf' &&
+    onBehalfString != '0xe8599f3cc5d38a9ad6f3684cd5cea72f10dbc383' &&
+    onBehalfString != '0xbe85413851d195fc6341619cd68bfdc26a25b928' &&
+    onBehalfString != '0x5ba7fd868c40c16f7adfae6cf87121e13fc2f7a0' &&
+    onBehalfString != '0x8a020d92d6b119978582be4d3edfdc9f7b28bf31' &&
+    onBehalfString != '0x053d55f9b5af8694c503eb288a1b7e552f590710' &&
+    onBehalfString != '0x464c71f6c2f760dda6093dcb91c24c39e5d6e18c'
+
+  if (isMintingToTreasury) {
+    const aave = loadAave()!
+    const userBalanceChange = value.minus(balanceIncrease)
+
+    aave.osTokenTotalSupplied = aave.osTokenTotalSupplied.plus(userBalanceChange)
+    aave.save()
+  }
+}
+
+function burn(value: BigInt, balanceIncrease: BigInt): void {
   const aave = loadAave()!
-  aave.osTokenSupplyCap = event.params.newSupplyCap
+  const userBalanceChange = value.plus(balanceIncrease)
+
+  aave.osTokenTotalSupplied = aave.osTokenTotalSupplied.minus(userBalanceChange)
   aave.save()
 }
 
-export function handleTransfer(event: Transfer): void {
-  const from = event.params.from
-  const to = event.params.to
-  const amount = event.params.value
+export function handleMint(event: Mint): void {
+  mint(event.params.value, event.params.balanceIncrease, event.params.onBehalfOf)
+}
 
-  if (amount.isZero()) {
-    return
+export function handleBurn(event: Burn): void {
+  burn(event.params.value, event.params.balanceIncrease)
+}
+
+let RAY = BigInt.fromI32(10).pow(27)
+let halfRAY = RAY.div(BigInt.fromI32(2))
+
+function rayMul(a: BigInt, b: BigInt): BigInt {
+  return a.times(b).plus(halfRAY).div(RAY)
+}
+
+export function handleTransfer(event: BalanceTransfer): void {
+  let balanceTransferValue = event.params.value
+
+  if (event.block.number.toU32() > 0) {
+    balanceTransferValue = rayMul(balanceTransferValue, event.params.index)
   }
 
-  const aave = loadAave()!
-
-  if (from.equals(Address.zero())) {
-    aave.osTokenTotalSupplied = aave.osTokenTotalSupplied.plus(amount)
-  }
-  if (to.equals(Address.zero())) {
-    aave.osTokenTotalSupplied = aave.osTokenTotalSupplied.minus(amount)
-  }
-
-  aave.save()
+  burn(balanceTransferValue, BigInt.zero())
+  mint(balanceTransferValue, BigInt.zero(), event.params.to)
 }
