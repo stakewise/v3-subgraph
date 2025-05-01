a9f0c1a8db24e40232e17f02ca0460c954597659
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2025-05-01 17:18:19 +0400

    Fix redundant hash property (#158)

--- src/mappings/merkleDistributor.ts
+++ src/mappings/merkleDistributor.ts
@@ -49,7 +49,6 @@ export function handlePeriodicDistributionAdded(event: PeriodicDistributionAdded
   }
 
   const distribution = new PeriodicDistribution(`${event.transaction.hash.toHex()}-${event.logIndex.toString()}`)
-  distribution.hash = event.transaction.hash
   distribution.distributionType = convertDistributionTypeToString(distType)
   distribution.data = extraData
   distribution.token = token
